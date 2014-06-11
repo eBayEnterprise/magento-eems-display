a3cd67d82e3979d098b95eb989f81e871ac8f70a
Reginald Gabriel <rgabriel@ebay.com>
2014-06-11 14:50:16 -0400

    US12942 Adding Copyright and License
    Created LICENSE.md and FILE_LICENSE.txt file. Use content
    in FILE_LICENSE.txt file to append license comments on .php script
    files, .xml files and .phtml files.

--- src/app/code/community/EbayEnterprise/Display/Test/Helper/DataTest.php
+++ src/app/code/community/EbayEnterprise/Display/Test/Helper/DataTest.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Copyright (c) 2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the eBay Enterprise
+ * Magento Extensions End User License Agreement
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.ebayenterprise.com/files/pdf/Magento_Connect_Extensions_EULA_050714.pdf
+ *
+ * @copyright   Copyright (c) 2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://www.ebayenterprise.com/files/pdf/Magento_Connect_Extensions_EULA_050714.pdf  eBay Enterprise Magento Extensions End User License Agreement
+ *
+ */
+
 class EbayEnterprise_Display_Test_Helper_DataTest extends EcomDev_PHPUnit_Test_Case
 {
 	/**
@@ -24,10 +40,10 @@ class EbayEnterprise_Display_Test_Helper_DataTest extends EcomDev_PHPUnit_Test_C
 	{
 		$content = '
 
-This is a string with 
+This is a string with
 multiple lines
 break to prove the
-the clean helper method will normalize the string 
+the clean helper method will normalize the string
 accordingly
 
 
--- src/app/code/community/EbayEnterprise/Display/controllers/IndexController.php
+++ src/app/code/community/EbayEnterprise/Display/controllers/IndexController.php
@@ -1,5 +1,21 @@
 <?php
 /**
+ * Copyright (c) 2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the eBay Enterprise
+ * Magento Extensions End User License Agreement
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.ebayenterprise.com/files/pdf/Magento_Connect_Extensions_EULA_050714.pdf
+ *
+ * @copyright   Copyright (c) 2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://www.ebayenterprise.com/files/pdf/Magento_Connect_Extensions_EULA_050714.pdf  eBay Enterprise Magento Extensions End User License Agreement
+ *
+ */
+
+/**
  * There is no index action for this controller. You must use 'retrieve?id=' to retrieve
  * an existing feed. If we decide later we want to create feeds upon request via this
  * controller, just add another action.
