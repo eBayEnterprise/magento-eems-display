a528ee3c3f8ce73cdd487ce5d299493fdfd50a6f
Chris Kemmerer <ckemmerer@ebay.com>
2014-11-17 22:15:53 -0500

    Update Unit Tetst For Jenkins

--- src/app/code/community/EbayEnterprise/Display/Test/Helper/DataTest.php
+++ src/app/code/community/EbayEnterprise/Display/Test/Helper/DataTest.php
@@ -81,9 +81,9 @@ class EbayEnterprise_Display_Test_Helper_DataTest extends EcomDev_PHPUnit_Test_C
 			array($fixtureDir . DIRECTORY_SEPARATOR . 'not-an-image.jpg', 0, 0, false),
 			array($fixtureDir . DIRECTORY_SEPARATOR . 'not-an-image.jpg', 150, 150, false),
 			array('no-file.jpg', 0, 0, false),
-			array('http://localhost/'. $relDir . DIRECTORY_SEPARATOR . 'test-product-img-150-150.jpg', 150, 150, true),
-			array('http://localhost/'. $relDir . DIRECTORY_SEPARATOR . 'no-file.jpg', 150, 150, false),
-			array('http://localhost/'. $relDir . DIRECTORY_SEPARATOR, 150, 150, false),
+			array('http://example.com/'. $relDir . DIRECTORY_SEPARATOR . 'test-product-img-150-150.jpg', 150, 150, true),
+			array('http://example.com/'. $relDir . DIRECTORY_SEPARATOR . 'no-file.jpg', 150, 150, false),
+			array('http://example.com/'. $relDir . DIRECTORY_SEPARATOR, 150, 150, false),
 			array('http://some.other.host/'. $relDir . DIRECTORY_SEPARATOR . 'test-product-img-150-150.jpg', 150, 150, true),
 		);
 	}
